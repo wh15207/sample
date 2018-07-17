ebac1661db8a52ae8b2a80f9c937a1fb5f000460
wh15207 <wh15207@163.com>
2018-07-17 01:44:07 +0000

    Finsh layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{route('home')}}" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('help') }}">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
