2f85c0185f2982490fedfcbfd7c7589c13a28955
wh15207 <wh15207@163.com>
2018-07-18 02:07:51 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'wh15207@163.com';
         $user->password = bcrypt('123456');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
